82d78848ed44323615b67c83bbfdad46c7fc6d45
Daniel Topp <dmtopp@gmail.com>
2016-04-18 11:16:24 -0500

    still fixed merge conflicts

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,14 @@
 var gulp        = require('gulp'),
-<<<<<<< HEAD
-    server      = require('gulp-webserver'),
-=======
->>>>>>> react
     tap         = require('gulp-tap'),
     fs          = require('fs'),
     browserify  = require('browserify'),
     buffer      = require('gulp-buffer'),
     gutil       = require('gulp-util'),
     uglify      = require('gulp-uglify'),
-<<<<<<< HEAD
     gls         = require('gulp-live-server'),
     less        = require('gulp-less'),
-    path        = require('path');
+    path        = require('path')
+    concat      = require('gulp-concat');;
 
 // Transpile LESS to CSS
 gulp.task('less', function() {
@@ -20,13 +16,6 @@ gulp.task('less', function() {
     .pipe(less())
     .pipe(gulp.dest('./public/css'));
 });
-
-// Watch for changes = first variable and run defined tasks = second variable
-gulp.task('watch', function() {
-  gulp.watch(['./public/less/**/*.less'], ['less']);
-});
-=======
-    concat      = require('gulp-concat');
 
 gulp.task('react', function(){
   return gulp.src('./public/jsx/*.js') // place to read react files from
@@ -39,9 +28,10 @@ gulp.task('react', function(){
     .pipe(gulp.dest('./public/js')); // place to send uglified regular js files
 });
 
+// Watch for changes = first variable and run defined tasks = second variable
 gulp.task('watch', function() {
   gulp.watch(['./public/jsx/*.js'], ['react']); // place to watch for react changes
+  gulp.watch(['./public/less/**/*.less'], ['less']);
 });
 
-gulp.task('default', ['react']);
->>>>>>> react
+gulp.task('default', ['react', 'less']);
